fda06da8670241041b8584dc373fae876d15e413
bdasgupta02 <bdasgupta02@gmail.com>
2021-09-20 10:03:47 +0800

    init text, icon, color, icon, microcopy files

---
+++ src/components/GigListingTile/index.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react'
+
+const GigListingTile = (props) => {
+    const [isHovering, setIsHovering] = useState(false)
+
+    return (
+        <div></div>
+    )
+}
+
+export default GigListingTile;
