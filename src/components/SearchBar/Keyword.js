e45fc2558137e918b8aa948a9aed48f960105c19
bdasgupta02 <bdasgupta02@gmail.com>
2021-10-02 08:49:12 +0800

    added first ver search bar (positioning)

---
+++ src/components/SearchBar/Keyword.js
@@ -0,0 +1,19 @@
+import './searchBar.css'
+
+/*
+Props:
+- keyword
+*/
+const Keyword = (props) => {
+    // TODO: (x) button handling
+
+    
+
+    return (
+        <div id="Keyword">
+            {text}
+        </div>
+    )
+}
+
+export default Keyword;
