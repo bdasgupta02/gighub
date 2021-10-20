a6a245417a21c2aa7f819073fbe96cf316454c52
bdasgupta02 <bdasgupta02@gmail.com>
2021-10-20 13:53:01 +0800

    fix npm ver and peer dependencies

--- src/database/firebase.js
+++ src/database/firebase.js
@@ -1,10 +1,11 @@
 // Import the functions you need from the SDKs you need
-
+import firebase from "firebase/compat/app";
 import { initializeApp } from "firebase/app";
 
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "@firebase/firestore";
 
+import "firebase/compat/auth"
 // TODO: Add SDKs for Firebase products that you want to use
 
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -15,30 +16,21 @@ import { getFirestore } from "@firebase/firestore";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 const firebaseConfig = {
-
   apiKey: "AIzaSyC55_bD19wHScf6SFl0QrgDXpgxC8CWgOs",
-
   authDomain: "gighub-c8dcf.firebaseapp.com",
-
   projectId: "gighub-c8dcf",
-
   storageBucket: "gighub-c8dcf.appspot.com",
-
   messagingSenderId: "600430772218",
-
   appId: "1:600430772218:web:46d53c414e5d0fff76ee43",
-
   measurementId: "G-8QPFC4QLR6"
-
 };
 
 
 // Initialize Firebase
 
-const app = initializeApp(firebaseConfig);
-
+const app = firebase.initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-
 const db = getFirestore(app);
 
+export const auth = app.auth()
 export default db;
