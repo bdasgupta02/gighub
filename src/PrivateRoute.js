eac5770d04288d6983a4111e651c5f54a223a10f
bdasgupta02 <bdasgupta02@gmail.com>
2021-10-20 16:21:11 +0800

    add working private route

---
+++ src/PrivateRoute.js
@@ -0,0 +1,22 @@
+import React, { useContext } from 'react'
+import { Route, Redirect } from 'react-router-dom'
+import { useAuth, AuthContext } from './Auth'
+
+const PrivateRoute = ({ component: RouteComponent, ...rest }) => {
+    const auth = useAuth()
+    const currentUser = auth === undefined ? null : auth.currentUser
+    return (
+        <Route
+            {...rest}
+            render={routeProps =>
+                !!currentUser ? (
+                    <RouteComponent {...routeProps} />
+                ) : (
+                    <Redirect to={'/signin'} />
+                )
+            }
+        />
+    )
+}
+
+export default PrivateRoute;
