f593af2c4b17acae757782d19e958925014e6df1
Sean <sean_sang@outlook.com>
2021-11-10 13:58:51 +0800

    Added FileUploader component

---
+++ src/components/FileUploader/index.js
@@ -0,0 +1,67 @@
+import {useState} from 'react';
+import { FileType } from '../../enum/FileType';
+import './fileUploader.css';
+//import { Modal } from '@material-ui/core';
+
+/**
+ * 
+ * @param {React.Dispatch<any>} setFileOutput
+ * @param {FileType} fileTypeEnum
+ * @returns 
+ */
+// export const FileUploader = ({}, fileOutput) => {
+//     const [file, setFile] = useState(null);
+
+//     const onInputChange = (e) => {
+//         setFile(e.target.files[0]);
+//     };
+//     const onSubmit = (e) => {
+//         e.preventDefault();
+//         fileOutput(file)
+
+//     }
+//     return (
+//         <form method="post" action="#" id="#" onSubmit={onSubmit}>
+//               <div class="form-group files color">
+//                 <label>Upload Your File (max size 2MB)</label>
+//                 <input type="file" class="form-control" multiple="false" onChange={onInputChange}/>
+//               </div>
+//               <button>Submit</button>
+//           </form>
+//     )
+// };
+export function FileUploader(setFileOutput, fileTypeEnum) {
+    const [file, setFile] = useState(null);
+
+    const onInputChange = (e) => {
+        setFile(e.target.files[0]);
+    };
+
+    const onSubmit = (e) => {
+        setFileOutput(file);
+    }
+
+    <form action="#" id="#" onSubmit={onSubmit}>
+        <div class="form-group files color">
+            <label>Upload Your File</label>
+            <input type="file" class="form-control" multiple="false" onChange={onInputChange} accept={fileTypeEnum}/>
+        </div>
+        <button id="submit">Submit</button>
+
+    </form>
+}
+
+/**
+ * @param {File} file 
+ * @returns Boolean whether or not the file is less than 2MB
+ */
+function validateSize(file, maxFileSize) {
+    let fileSize = Math.round(file.size/1024/1024);
+
+    if (fileSize >= maxFileSize) {
+        alert("File too big, please select a file less than 2MB");
+        return false;
+    } else {
+        return true;
+    }
+}
---
+++ src/enum/FileType.js
@@ -0,0 +1,10 @@
+/**
+ * PICTURES will allow for only .png and .jpeg
+ * DOCUMENTS will only allow .pdf
+ * ALL accepts any file type
+ */
+export const FileType = {
+    PICTURES: ".png, .jpeg",
+    DOCUMENTS: ".pdf",
+    ALL: ""
+}
