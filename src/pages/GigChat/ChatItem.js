795a709275b449a41c16335668ade60f39793a3c
bdasgupta02 <bdasgupta02@gmail.com>
2021-11-12 02:35:40 +0800

    built and finished chat

--- src/pages/GigChat/ChatItem.js
+++ src/pages/GigChat/ChatItem.js
@@ -1,9 +1,27 @@
 import React from 'react'
+import { Col, Row } from 'react-grid-system'
 import './gigChat.css'
 
-function ChatItem() {
+function ChatItem(props) {
+    const { isSender, content, timeString, logo } = props
+
+    const outerClass = isSender ? "CIOuterBoxCommon CIOuterBoxSend" : "CIOuterBoxCommon CIOuterBoxRec"
     return (
-        <div>
+        <div className="CIBox">
+            {isSender ? null : (
+                <div style={{marginRight: '16px'}}>
+                    {logo}
+                </div>
+            )}
+            <div className={outerClass}>
+                <div style={{ width: '100%' }}>
+                    {content}
+                </div>
+                <div style={{ width: '100%', height: '16px' }} />
+                <div style={{ width: '100%' }} className="CIInnerDateString">
+                    {timeString}
+                </div>
+            </div>
             
         </div>
     )
